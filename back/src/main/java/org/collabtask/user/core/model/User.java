30f1413a7d0dc44acafec8fc0e1937951eb1bf13
heryfitiavana22 <hheryfitiavana@gmail.com>
2025-03-04 11:50:50 +0300

    Add task assignment and creation lists to User model

--- back/src/main/java/org/collabtask/user/core/model/User.java
+++ back/src/main/java/org/collabtask/user/core/model/User.java
@@ -1,14 +1,22 @@
 package org.collabtask.user.core.model;
 
+import java.util.List;
+
+import org.collabtask.task.core.model.Task;
+
 public class User {
     private String id;
     private String username;
     private String email;
+    private List<Task> tasksAssigned;
+    private List<Task> tasksCreated;
 
-    public User(String id, String username, String email) {
+    public User(String id, String username, String email, List<Task> tasksAssigned, List<Task> tasksCreated) {
         this.id = id;
         this.username = username;
         this.email = email;
+        this.tasksAssigned = tasksAssigned;
+        this.tasksCreated = tasksCreated;
     }
 
     public String getId() {
@@ -33,5 +41,21 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Task> getTasksAssigned() {
+        return tasksAssigned;
+    }
+
+    public void setTasksAssigned(List<Task> tasksAssigned) {
+        this.tasksAssigned = tasksAssigned;
+    }
+
+    public List<Task> getTasksCreated() {
+        return tasksCreated;
+    }
+
+    public void setTasksCreated(List<Task> tasksCreated) {
+        this.tasksCreated = tasksCreated;
     }
 }
