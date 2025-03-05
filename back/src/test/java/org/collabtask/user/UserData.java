de4cbdd5e291690aa46dca1749d15ceaeda55826
heryfitiavana22 <hheryfitiavana@gmail.com>
2025-03-05 12:24:18 +0300

    Add UserData class and enhance UserRepositoryTest with data initialization for testing

---
+++ back/src/test/java/org/collabtask/user/UserData.java
@@ -0,0 +1,34 @@
+package org.collabtask.user;
+
+import org.collabtask.user.core.dto.CreateUser;
+import org.collabtask.user.database.UserEntity;
+
+import io.smallrye.mutiny.Uni;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class UserData {
+    private UserEntity userEntity1;
+    private UserEntity userEntity2;
+
+    public UserData() {
+    }
+
+    public Uni<String> init() {
+        userEntity1 = new UserEntity();
+        userEntity1.setUsername("username1");
+        userEntity1.setEmail("username1@gmail.com");
+
+        userEntity2 = new UserEntity();
+        userEntity2.setUsername("usernam2");
+        userEntity2.setEmail("username2@gmail.com");
+
+        return userEntity1.persistAndFlush()
+                .chain(() -> userEntity2.persistAndFlush())
+                .replaceWith(() -> "Users created");
+    }
+
+    public CreateUser createUser() {
+        return new CreateUser("username", "email@gmail.com");
+    }
+}
--- back/src/test/java/org/collabtask/user/UserRepositoryTest.java
+++ back/src/test/java/org/collabtask/user/UserRepositoryTest.java
@@ -1,5 +1,29 @@
 package org.collabtask.user;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.collabtask.user.database.UserEntityRepository;
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.TestReactiveTransaction;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.vertx.UniAsserter;
+import jakarta.inject.Inject;
+
+@QuarkusTest
+@TestReactiveTransaction
 public class UserRepositoryTest {
+    @Inject
+    UserData userData;
     
+    UserEntityRepository userRepository = new UserEntityRepository();
+    
+    @Test
+    void shouldFindAll(UniAsserter asserter) {
+        asserter.execute(() -> userData.init())
+                .assertThat(() -> userRepository.findAll(), (data) -> {
+                    assertNotNull(data);
+                });
+
+    }
 }
