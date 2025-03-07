20810c4b4a688810079ff42b8af83de22c5cfa58
heryfitiavana22 <hheryfitiavana@gmail.com>
2025-03-07 11:13:32 +0300

    Add user repository tests for findById and findByUsername methods; include error handling for non-existent users

--- back/src/test/java/org/collabtask/user/UserRepositoryTest.java
+++ back/src/test/java/org/collabtask/user/UserRepositoryTest.java
@@ -1,9 +1,11 @@
 package org.collabtask.user;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.collabtask.helpers.Pagination;
+import org.collabtask.user.core.exception.UserNotFoundException;
 import org.collabtask.user.database.UserEntityRepository;
 import org.junit.jupiter.api.Test;
 
@@ -17,9 +19,9 @@ import jakarta.inject.Inject;
 public class UserRepositoryTest {
     @Inject
     UserData userData;
-    
+
     UserEntityRepository userRepository = new UserEntityRepository();
-    
+
     @Test
     void shouldFindAll(UniAsserter asserter) {
         asserter.execute(() -> userData.init())
@@ -29,5 +31,39 @@ public class UserRepositoryTest {
                     assertTrue(data.getData().size() > 0);
                 });
 
+    }
+
+    @Test
+    public void shouldFindById(UniAsserter asserter) {
+        asserter.execute(() -> userData.init())
+                .assertThat(() -> userRepository.findById(userData.getUserEntity1().id), post -> {
+                    assertNotNull(post);
+                    assertEquals(userData.getUserEntity1().id, post.getId());
+                });
+
+    }
+
+    @Test
+    void shouldNotFoundIfIdNotExist(UniAsserter asserter) {
+        asserter.execute(() -> userData.init())
+                .assertFailedWith(() -> userRepository.findById("ndksalknk"),
+                        throwable -> assertTrue(throwable instanceof UserNotFoundException));
+    }
+
+    @Test
+    public void shouldFindByUsername(UniAsserter asserter) {
+        asserter.execute(() -> userData.init())
+                .assertThat(() -> userRepository.findById(userData.getUserEntity1().getUsername()), post -> {
+                    assertNotNull(post);
+                    assertEquals(userData.getUserEntity1().getUsername(), post.getUsername());
+                });
+
+    }
+
+    @Test
+    void shouldNotFoundIfUsernameNotExist(UniAsserter asserter) {
+        asserter.execute(() -> userData.init())
+                .assertFailedWith(() -> userRepository.findByUsername("roigjnfk"),
+                        throwable -> assertTrue(throwable instanceof UserNotFoundException));
     }
 }
