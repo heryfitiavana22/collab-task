008567a76b5056920b6e8353c39159e8400bb555
heryfitiavana22 <hheryfitiavana@gmail.com>
2025-03-20 15:35:43 +0300

    Add unit tests for TaskService findById method and handle non-existent task scenario

--- back/src/test/java/org/collabtask/task/TaskServiceTest.java
+++ back/src/test/java/org/collabtask/task/TaskServiceTest.java
@@ -1,8 +1,16 @@
 package org.collabtask.task;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.collabtask.task.core.exception.TaskNotFoundException;
 import org.collabtask.task.core.service.TaskService;
+import org.junit.jupiter.api.Test;
+
 import io.quarkus.test.TestReactiveTransaction;
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.vertx.UniAsserter;
 import jakarta.inject.Inject;
 
 @QuarkusTest
@@ -13,4 +21,21 @@ public class TaskServiceTest {
 
     @Inject
     TaskService taskService;
+
+    @Test
+    void shouldFindById(UniAsserter asserter) {
+        asserter.execute(() -> taskData.init())
+                .assertThat(() -> taskService.findById(taskData.getTaskTodoNormale().getId()),
+                        find -> {
+                            assertNotNull(find);
+                            assertEquals(taskData.getTaskTodoNormale().getTitle(), find.getTitle());
+                        });
+    }
+
+    @Test
+    void shouldNotFindByIdIfTaskIdDoesNotExist(UniAsserter asserter) {
+        asserter.execute(() -> taskData.init())
+                .assertFailedWith(() -> taskService.findById("nonexistentTaskId"),
+                        throwable -> assertTrue(throwable instanceof TaskNotFoundException));
+    }
 }
