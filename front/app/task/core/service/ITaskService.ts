2fd2478d3454af134930e152575fcd5b5fa6b173
heryfitiavana22 <hheryfitiavana@gmail.com>
2025-03-23 16:01:09 +0300

    refactor: Replace Error with HttpError in ITaskService and IUserService interfaces

--- front/app/task/core/service/ITaskService.ts
+++ front/app/task/core/service/ITaskService.ts
@@ -1,9 +1,10 @@
+import type { HttpError } from "~/lib/http/http-errors";
 import type { CreateTask, PaginatedTask, Task, UpdateTask } from "../task";
 import type { Result } from "neverthrow";
 
 export interface ITaskService {
-  findAll(): Promise<Result<PaginatedTask, Error>>;
-  findById(id: string): Promise<Result<Task, Error>>;
-  create(createTask: CreateTask): Promise<Result<Task, Error>>;
-  update(updatTask: UpdateTask): Promise<Result<Task, Error>>;
+  findAll(): Promise<Result<PaginatedTask, HttpError>>;
+  findById(id: string): Promise<Result<Task, HttpError>>;
+  create(createTask: CreateTask): Promise<Result<Task, HttpError>>;
+  update(updatTask: UpdateTask): Promise<Result<Task, HttpError>>;
 }
--- front/app/user/core/service/IUserService.ts
+++ front/app/user/core/service/IUserService.ts
@@ -1,10 +1,11 @@
 import type { Result } from "neverthrow";
 import type { CreateUser, PaginatedUser, User } from "../user";
+import type { HttpError } from "~/lib/http/http-errors";
 
 export interface IUserService {
-  findAll(): Promise<Result<PaginatedUser, Error>>;
-  findById(id: string): Promise<Result<User, Error>>;
-  findByEmail(email: string): Promise<Result<User, Error>>;
-  create(createUser: CreateUser): Promise<Result<User, Error>>;
-  deleteById(id: string): Promise<Result<User, Error>>;
+  findAll(): Promise<Result<PaginatedUser, HttpError>>;
+  findById(id: string): Promise<Result<User, HttpError>>;
+  findByEmail(email: string): Promise<Result<User, HttpError>>;
+  create(createUser: CreateUser): Promise<Result<User, HttpError>>;
+  deleteById(id: string): Promise<Result<User, HttpError>>;
 }
