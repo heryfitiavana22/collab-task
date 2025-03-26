f23586784e7eb72d50361f8836160db9b7f06c2d
heryfitiavana22 <hheryfitiavana@gmail.com>
2025-03-26 17:59:32 +0300

    feat: Update task and user services to use backend implementations with HTTP client

--- front/app/task/core/service/index.ts
+++ front/app/task/core/service/index.ts
@@ -1,10 +1,14 @@
 import { userService } from "~/user/core/service";
 import type { ITaskService } from "./ITaskService";
 import { TaskServiceInMemory } from "./TaskServiceInMemory";
+import { TaskServiceBackend } from "./TaskServiceBackend";
+import { httpClient } from "~/lib/http/http-client";
 
 function factory(source: "InMemory" | "quarkus"): ITaskService {
   if (source == "InMemory") return new TaskServiceInMemory(userService);
+  if (source == "quarkus")
+    return new TaskServiceBackend(httpClient.withPrefix("/tasks"));
   throw new Error("Unknown source task service");
 }
 
-export const taskService = factory("InMemory");
+export const taskService = factory("quarkus");
--- front/app/user/core/service/index.ts
+++ front/app/user/core/service/index.ts
@@ -1,9 +1,13 @@
+import { httpClient } from "~/lib/http/http-client";
 import type { IUserService } from "./IUserService";
+import { UserServiceBackend } from "./UserServiceBackend";
 import { UserServiceInMemory } from "./UserServiceInMemory";
 
 function factory(src: "InMemory" | "quarkus"): IUserService {
   if (src == "InMemory") return new UserServiceInMemory();
+  if (src == "quarkus")
+    return new UserServiceBackend(httpClient.withPrefix("/users"));
   throw new Error("Unknown source user service");
 }
 
-export const userService = factory("InMemory");
+export const userService = factory("quarkus");
